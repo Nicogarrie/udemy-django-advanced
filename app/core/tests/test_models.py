26b658b0fdc7dcc260d0aeda76739884438abdac
Nicolas <nicogarrie@gmail.com>
2022-04-07 20:18:45 +0200

    Fixed flake8 space error

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,7 +7,7 @@ class ModelTests(TestCase):
     def test_create_user_with_email_succesful(self):
         """Test creating a new user with an email is successful"""
         email = 'test@gmail.com'
-        password  = 'test1234'
+        password = 'test1234'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
