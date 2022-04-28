3eb9054d21716cb3eabf0776cb0aacd1919642d6
Nicolas <nicogarrie@gmail.com>
2022-04-28 20:51:15 +0200

    Added get ingredients feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -19,7 +19,7 @@ class PublicTagsApiTests(TestCase):
         self.client = APIClient()
 
     def test_login_required(self):
-        """Test thath login is required for retrieving tags"""
+        """Test that login is required for retrieving tags"""
         res = self.client.get(TAGS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
